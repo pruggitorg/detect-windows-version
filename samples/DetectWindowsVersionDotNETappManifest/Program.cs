f0cc1e2609ef4edbbaf46a350a0e7e927f8f4eab
Stefan Prugg <stefan.prugg@prugg.at>
2022-09-17 08:49:53 +0200

    Update regarding Windows 11

--- samples/DetectWindowsVersionDotNETappManifest/Program.cs
+++ samples/DetectWindowsVersionDotNETappManifest/Program.cs
@@ -14,8 +14,10 @@ namespace DetectWindowsVersionDotNETappManifest
             // In Visual Studio you can add an app manifest by doing the following:
             // Go to your project, right click and choose Add / New Item and choose Application Manifest File.
             // A new file will be added to your project having default name app.manifest. In the 
-            // section <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1"> you can uncomment Windows 8.1 and 10.
-            // If you run the program again, the output on a Windows 10 will be correct.
+            // section <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1"> you can uncomment Windows 8.1 and 10/11.
+            // If you run the program again, the output on a Windows 10/Windows 11 will be correct.
+            //
+            // see also: https://learn.microsoft.com/en-us/windows/win32/sysinfo/targeting-your-application-at-windows-8-1
 
             Console.WriteLine($"" +
             $"{System.Environment.OSVersion.Version.Major}." +
